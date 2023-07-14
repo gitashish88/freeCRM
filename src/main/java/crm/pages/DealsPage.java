f27516acd2bbc93ce8f6b65e79a7ed0daf601d28
gitashish88 <a.r.sharma111@gmail.com>
2023-07-15 00:05:42 +0530

    added deals page changes

--- src/main/java/crm/pages/DealsPage.java
+++ src/main/java/crm/pages/DealsPage.java
@@ -1,5 +1,13 @@
 package crm.pages;
 
-public class DealsPage {
+import org.openqa.selenium.support.PageFactory;
 
+import crm.core.WebDriverBase;
+
+public class DealsPage extends WebDriverBase {
+
+	public DealsPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
 }
